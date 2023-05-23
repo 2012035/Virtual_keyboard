790b69d82c8796aac855f1100559fbd8659665bd
2012035 <114689396+2012035@users.noreply.github.com>
2023-05-23 08:29:47 +0530

    mouse_control

---
+++ Virtual_mouse_control/mouse_control.py
@@ -0,0 +1,55 @@
+import cv2
+import mediapipe as mp
+import time
+import math
+import numpy as np
+import handTracking as ht
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+pTime=0
+cTime=0
+
+
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = interface.QueryInterface(IAudioEndpointVolume)
+vol=volume.GetVolumeRange()
+minvol=vol[0]
+maxvol=vol[1]
+
+
+
+video=cv2.VideoCapture(0)
+video.set(3,480)
+video.set(4,680)
+video.set(10,150)
+detection=ht.handDetector(detection=0.8)
+while True:
+    s,image=video.read()
+    image=detection.handDetection(image,draw=False)
+    list=detection.findPosition(image,draw=False)
+    if len(list)!=0:
+        x,y=list[4][1],list[4][2]
+        x2,y2=list[8][1],list[8][2]
+        cv2.circle(image,(x,y),5,(255,0,0),cv2.FILLED)
+        cv2.circle(image, (x2, y2), 5, (255, 0, 0),cv2.FILLED)
+        cv2.line(image,(x,y),(x2,y2),(255,0,0),4)
+        cv2.circle(image,(int((x+x2)/2),int((y+y2)/2)),10,(255,230,0),cv2.FILLED)
+        lenght=math.sqrt((x2-x)**2+(y2-y)**2)
+        if (lenght<=30):
+
+            cv2.circle(image,(int((x+x2)/2),int((y+y2)/2)),10,(0,255,0),cv2.FILLED)
+        vollen=np.interp(lenght,[20,180],[minvol,maxvol])
+        volume.SetMasterVolumeLevel(vollen, None)
+
+
+
+    cTime=time.time()
+    fps=1/(cTime-pTime)
+    pTime=cTime
+    cv2.putText(image,str(int(fps)),(10,70) ,cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 2)
+    cv2.imshow('Mouse Controll',image)
+    if(cv2.waitKey(1) & 0xFF==ord('z')):
+        break
